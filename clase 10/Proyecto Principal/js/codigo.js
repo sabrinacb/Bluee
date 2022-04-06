fcdbdf83a3be63bbde96ea4f80b5c8f013880682
sabrinacb <79062119+sabrinacb@users.noreply.github.com>
2022-04-05 22:00:57 -0300

    Add files via upload

---
+++ clase 10/Proyecto Principal/js/codigo.js
@@ -0,0 +1,16 @@
+function abrir(){
+
+
+document.getElementById('overlay').style.display = 'block';
+
+
+}
+
+
+function cerrar(){
+
+
+    document.getElementById('overlay').style.display = 'none';
+    
+    
+    }
