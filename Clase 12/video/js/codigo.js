3a3d5f048db8550787417e78cef2eb18305a7bd2
sabrinacb <79062119+sabrinacb@users.noreply.github.com>
2022-04-12 22:12:07 -0300

    Add files via upload

---
+++ Clase 12/video/js/codigo.js
@@ -0,0 +1,28 @@
+function reproducir(){
+
+document.getElementById('video').play();
+
+}
+
+function parar(){
+
+    document.getElementById('video').pause();
+
+}
+
+
+function cambiar2(){
+
+
+    document.getElementById('video').src = 'videos/Video02.mp4'
+
+
+}
+
+function cambiar1(){
+
+
+    document.getElementById('video').src = 'videos/Video01.mp4'
+
+
+}
