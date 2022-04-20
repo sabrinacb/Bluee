c872283489bd363a6452b93301462da6ce00ab51
sabrinacb <79062119+sabrinacb@users.noreply.github.com>
2022-04-19 22:04:49 -0300

    Add files via upload

---
+++ Clase 13/formulario/php/codigo.php
@@ -0,0 +1,13 @@
+<?php
+
+//comentario
+/*comentario
+de varias lineas*/
+/*
+echo "Hola!, ¿como estás?";
+*/
+
+echo "<h1>hola como estás "." ".$_POST["nombre"]." sé que naciste el ".$_POST["nacimiento"]."y tu pais de origen es: ".$_POST["pais"]." y tu color preferido es ".$_POST["color"]."</h1>";
+
+
+?>
